6c53f3b77b42d0e4a04e4681f2c9abeb1a1e5f7d
omarhosny206 <omarhosny102@gmail.com>
2022-10-13 16:12:47 +0200

    Updated error handler

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,6 +1,8 @@
+const StatusCode = require("../utils/status-code");
+
 exports.handle = async (error, req, res, next) => {
   console.log("Message = ", error.message);
   console.log("Status = ", error.status);
-  error.status = error.status || 500;
+  error.status = error.status || StatusCode.INTERNAL_SERVER_ERROR;
   return res.status(error.status).json({ message: error.message });
 };
